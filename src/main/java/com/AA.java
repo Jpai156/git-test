ccade6f9e4f55e893a3af4d13e27399f2a0c76e9
jpai <2925108196@qq.com>
2022-05-08 11:45:41 +0800

    第一次提交

---
+++ src/main/java/com/AA.java
@@ -0,0 +1,15 @@
+package com;
+
+import java.util.Date;
+
+/**
+ * @author Jinp
+ * @Date 2022/5/8-10:58
+ */
+public class AA {
+
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+
+}
